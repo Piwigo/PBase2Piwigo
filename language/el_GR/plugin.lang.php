c4e25e1c0cbe336b21d71e965d17b53a6209eb2b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-08 22:32:51 +0000

    [pbase2piwigo] Update el_GR, thanks to : bas_alba
    git-svn-id: http://piwigo.org/svn/extensions/pbase2piwigo@17479 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Select albums to import'] = 'Επιλέξτε άλμπουμ για την εισαγωγή';
+$lang['Recursive'] = 'Αναδρομικά';
+$lang['Photo "%s" imported'] = 'Η φωτογραφία "%s" εισάγεται';
+$lang['PBase login'] = 'Σύνδεση με PBase ';
+$lang['No download method available. PBase2piwigo needs cURL extension or allow_url_fopen=true'] = 'Δεν υπάρχει διαθέσιμη μέθοδος λήψης. PBase2piwigo χρειάζεται την επέκταση cURL ή το allow_url_fopen = true';
+$lang['Nb photos'] = 'Nb φωτογραφίες';
+$lang['Nb albums'] = 'Nb λευκώματα';
+$lang['Invalid user id'] = 'Μη έγκυρο αναγνωριστικό  id χρήστη';
+$lang['Import queue is empty'] = 'Η ουρά εισαγωγής είναι άδεια';
+$lang['Import in this album'] = 'Εισαγωγή σε αυτό το λεύκωμα';
+$lang['Fill these fields from pBase datas'] = 'Συμπληρώστε τα πεδία από τα δεδομένα του pBase';
+$lang['Errors'] = 'Λάθη';
+$lang['Completed. %1$d albums and %2$d photos added.'] = 'Ολοκληρώθηκε. %1$d λευκώματα και %2$ φωτογραφίες προστέθηκαν.';
+$lang['Begin transfer'] = 'Ξεκινήστε μεταφορά';
+$lang['Begin analyse'] = 'Ξεκινήστε ανάλυση';
+$lang['Album "%s" created'] = 'Το λεύκωμα "%s" δημιουργήθηκε';
+$lang['%1$d errors occured. %2$d albums and %3$d photos added.'] = '%1$d σφάλματα εμφανίστηκαν. %2$d λευκώματα και %3$d φωτογραφίες προστέθηκαν.';
+?>
