8cf496720c4587fde83a111c4f445b34b117fef8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-10 23:05:56 +0000

    [pbase2piwigo] Update lv_LV, thanks to : agrisans
    git-svn-id: http://piwigo.org/svn/extensions/pbase2piwigo@17544 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['No download method available. PBase2piwigo needs cURL extension or allow_url_fopen=true'] = 'Lejupielādes metode nav pieejama. PBase2piwigo nepieciešams cURL paplašinājums vai allow_url_fopen=true';
+$lang['%1$d errors occured. %2$d albums and %3$d photos added.'] = 'Radušās %1$d kļūdas. Pievienoti %2$d albūmi un %3$d fotogrāfijas.';
+$lang['Completed. %1$d albums and %2$d photos added.'] = 'Pabeigts. Pievienoti %1$d albūmi un %2$d fotogrāfijas.';
+$lang['Fill these fields from pBase datas'] = 'Aizpildiet šos laukus no pBase datiem';
+$lang['Select albums to import'] = 'Atlasīt importējamos albūmus';
+$lang['Recursive'] = 'Rekursīvs';
+$lang['Photo "%s" imported'] = 'Importēta "%s" fotogrāfija';
+$lang['PBase login'] = 'PBase logins';
+$lang['Nb photos'] = 'Nb fotogrāfijas';
+$lang['Nb albums'] = 'Nb albūmi';
+$lang['Invalid user id'] = 'Kļūdains lietotāja id';
+$lang['Import queue is empty'] = 'Importa rinda ir tukša';
+$lang['Import in this album'] = 'Importēt šajā albūmā';
+$lang['Errors'] = 'Kļūda';
+$lang['Begin transfer'] = 'Sākt pārsūtīšanu';
+$lang['Begin analyse'] = 'Sākt analīzi';
+$lang['Album "%s" created'] = 'Albūms "%s" izveidots';
+?>
