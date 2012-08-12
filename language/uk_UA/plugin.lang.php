814639592d8454c8f03381793bf32ab636c3bd46
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-12 06:36:08 +0000

    [pbase2piwigo] Update uk_UA, thanks to : animan
    git-svn-id: http://piwigo.org/svn/extensions/pbase2piwigo@17552 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Fill these fields from pBase datas'] = 'Заповніть ці поля даних PBase';
+$lang['%1$d errors occured. %2$d albums and %3$d photos added.'] = '%1$d сталися помилок. %2$d альбомів і %3$d зображень додано.';
+$lang['No download method available. PBase2piwigo needs cURL extension or allow_url_fopen=true'] = 'Немає доступного методу завантаження. PBase2piwigo треба cURL розширення або allow_url_fopen=true';
+$lang['Album "%s" created'] = 'Альбом "%s" створений';
+$lang['Completed. %1$d albums and %2$d photos added.'] = 'Завершено. %1$d альбомів і %2$d зображень додано.';
+$lang['Import queue is empty'] = 'Черга на імпортування порожня';
+$lang['Photo "%s" imported'] = 'Зображення "%s" імпортовано';
+$lang['Begin analyse'] = 'Запустити аналіз';
+$lang['Begin transfer'] = 'Почати передачу';
+$lang['Errors'] = 'Помилки';
+$lang['Import in this album'] = 'Імпортувати в цей альбом';
+$lang['Invalid user id'] = 'Невірний ідентифікатор користувача';
+$lang['Nb albums'] = 'Кільк. альбомів';
+$lang['Nb photos'] = 'Кільк. зображень';
+$lang['PBase login'] = 'Лоґін PBase';
+$lang['Recursive'] = 'Рекурсивний';
+$lang['Select albums to import'] = 'Виберіть альбоми для імпорту';
+?>
