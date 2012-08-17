9d3d86dc5af84bce2915f9e9f781ab0a33a79f22
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-17 06:24:41 +0000

    [pbase2piwigo] Update tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/extensions/pbase2piwigo@17591 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Recursive'] = 'Yinelenebilir';
+$lang['No download method available. PBase2piwigo needs cURL extension or allow_url_fopen=true'] = 'İndirme yöntemi tanımlı değil. cURL eklentisi yada allow_url_fopen=true parametresi gereklidir.';
+$lang['%1$d errors occured. %2$d albums and %3$d photos added.'] = '%1$d hata oluştu. %2$d albüm ve %3$d fotoğraf eklendi.';
+$lang['Album "%s" created'] = '"%s" albümü yaratıldı.';
+$lang['Begin analyse'] = 'Analiz başlat';
+$lang['Begin transfer'] = 'Transfer başlat';
+$lang['Completed. %1$d albums and %2$d photos added.'] = 'Tamamlandı.%1$d albüm ve %2$d fotoğraf eklendi.';
+$lang['Errors'] = 'Hatalar';
+$lang['Fill these fields from pBase datas'] = 'pBase bilgilerinden bu alanları doldur';
+$lang['Import in this album'] = 'Bu albüm içine aktar';
+$lang['Import queue is empty'] = 'İçe aktarma kuyruğu boş';
+$lang['Invalid user id'] = 'Geçersiz kullanıcı kimliği';
+$lang['Nb albums'] = 'Nb albüm';
+$lang['Nb photos'] = 'Nb fotoğraf';
+$lang['PBase login'] = 'pBase oturumu';
+$lang['Photo "%s" imported'] = '"%s" fotoğrafı içe aktarıldı';
+$lang['Select albums to import'] = 'İçe aktarılacak albümleri seçin';
+?>
