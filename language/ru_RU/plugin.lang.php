dd759bd506239c2a5504b12151743b68125e662f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-20 06:31:42 +0000

    [pbase2piwigo] Update ru_RU, thanks to : yuragal
    git-svn-id: http://piwigo.org/svn/extensions/pbase2piwigo@17597 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,38 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%1$d errors occured. %2$d albums and %3$d photos added.'] = 'Добавлено альбомов: %2$d. Добавлено фотографий:%3$d. Произошло ошибок: %1$d.';
+$lang['Album "%s" created'] = 'Альбом "%s" создан';
+$lang['Begin analyse'] = 'Начать анализ';
+$lang['Begin transfer'] = 'Начать передачу';
+$lang['Completed. %1$d albums and %2$d photos added.'] = 'Завершено. Добавлено альбомов: %1$d. Добавлено фотографий:%2$d.';
+$lang['Errors'] = 'Ошибки';
+$lang['Fill these fields from pBase datas'] = 'Заполните эти поля согласно данным pBase';
+$lang['Import in this album'] = 'Импортировать в этот альбом';
+$lang['Import queue is empty'] = 'Очередь импорта пуста';
+$lang['Invalid user id'] = 'Неверный идентификатор пользователя';
+$lang['No download method available. PBase2piwigo needs cURL extension or allow_url_fopen=true'] = 'Отсутсвуют методы загрузки. Для работы PBase2piwigo необходимо расширение cURL или активированная опция allow_url_open=true.';
+$lang['PBase login'] = 'Логин PBase';
+$lang['Photo "%s" imported'] = 'Фотография "%s" импортирована';
+$lang['Recursive'] = 'Рекурсивно';
+$lang['Select albums to import'] = 'Выберите альбом для импорта';
+?>
