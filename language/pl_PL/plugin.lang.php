a3b552fbf89f4b4591c1cb4a00f1b2c142b5be2c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-27 18:43:04 +0000

    [pbase2piwigo] Update pl_PL, thanks to : Greybrow
    git-svn-id: http://piwigo.org/svn/extensions/pbase2piwigo@18312 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pl_PL/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Album "%s" created'] = 'Utworzono album "%s"';
+$lang['Begin analyse'] = 'Rozpocznij analizę';
+$lang['Begin transfer'] = 'Rozpocznij transfer';
+$lang['Completed. %1$d albums and %2$d photos added.'] = 'Import zakończony. Dodano %1$d albumów %2$d zdjęć.';
+$lang['Errors'] = 'Błędy';
+$lang['Fill these fields from pBase datas'] = 'Uzupełnij pola z danych PBase';
+$lang['Import in this album'] = 'Zaimportuj ten album';
+$lang['Import queue is empty'] = 'Kolejka importu jest pusta';
+$lang['Invalid user id'] = 'Nieprawidłowy id użytkownika';
+$lang['Nb albums'] = 'Liczba albumów';
+$lang['Nb photos'] = 'Liczba zdjęć';
+$lang['No download method available. PBase2piwigo needs cURL extension or allow_url_fopen=true'] = 'Import niemożliwy. PBase2piwigo potrzebuje na serwerze rozszerzenia cURL lub opcji allow_url_fopen=true';
+$lang['PBase login'] = 'Login PBase';
+$lang['Photo "%s" imported'] = 'Zdjęcie "%s" zaimportowane';
+$lang['Recursive'] = 'Rekursywnie';
+$lang['Select albums to import'] = 'Wybierz zdjęcia do zaimportowania';
+$lang['%1$d errors occured. %2$d albums and %3$d photos added.'] = 'Wystąpiło %1$d błędów. Dodano %2$d albumów i %3$d zdjęć.';
+?>
