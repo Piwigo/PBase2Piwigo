eb3eada73d8fd8de039f7e73921e7b80cf860b75
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-10-06 06:25:24 +0000

    [pbase2piwigo] Update cs_CZ, thanks to : sichr
    git-svn-id: http://piwigo.org/svn/extensions/pbase2piwigo@18498 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%1$d errors occured. %2$d albums and %3$d photos added.'] = 'Nastalo %1$d chyb. %2$d alb a %3$d fotografií přidáno.';
+$lang['Album "%s" created'] = 'Album "%s" vytvořeno';
+$lang['Begin analyse'] = 'Spustit analýzu';
+$lang['Begin transfer'] = 'Spustit přenos';
+$lang['Completed. %1$d albums and %2$d photos added.'] = 'Hotovo. %1$d alb a %2$d fotografií přidáno.';
+$lang['Errors'] = 'Chyby';
+$lang['Fill these fields from pBase datas'] = 'Vyplňte pole daty z pBase';
+$lang['Import in this album'] = 'Importovat toto album';
+$lang['Import queue is empty'] = 'Fronta importu je prázdná';
+$lang['Invalid user id'] = 'Neplatné uživatelské ID';
+$lang['Nb albums'] = 'Nb alb';
+$lang['Nb photos'] = 'Nb fotografií';
+$lang['No download method available. PBase2piwigo needs cURL extension or allow_url_fopen=true'] = 'Není k dispozici žádný způsob stahování. PBase2piwigo potřebuje cURL doplněk nebo allow_url_fopen=true';
+$lang['PBase login'] = 'PBase login';
+$lang['Photo "%s" imported'] = 'Foto "%s" naimportováno';
+$lang['Recursive'] = 'Zpětně';
+$lang['Select albums to import'] = 'Vyberte alba, která chcete importovat';
+?>
