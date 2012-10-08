8e30f1d9ab3d05cc6fe9fc7d8e708caa6ef85bf7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-10-08 16:06:43 +0000

    [pbase2piwigo] Update fa_IR, thanks to : arashshiler
    git-svn-id: http://piwigo.org/svn/extensions/pbase2piwigo@18554 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fa_IR/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Nb albums'] = 'تعداد آلبوم ها';
+$lang['Nb photos'] = 'تعداد تصاویر';
+$lang['PBase login'] = 'ورود به PBase';
+$lang['Photo "%s" imported'] = 'وارد کردن  "%s"عکس';
+$lang['Select albums to import'] = 'انتخاب آلبوم وارد کننده';
+$lang['Import in this album'] = 'وارد کردن به این آلبوم';
+$lang['Errors'] = 'خطاها';
+$lang['Begin transfer'] = 'اغاز انتقال';
+$lang['Album "%s" created'] = 'ایجاد  "%s" آلبوم';
+$lang['Begin analyse'] = 'آغاز تجزیه و تحلیل';
+?>
