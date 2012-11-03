feb08b293706922644f98b72b5589cbe654f5a54
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-11-03 18:47:07 +0000

    [pbase2piwigo] Add hu_HU, thanks to : evryka23
    git-svn-id: http://piwigo.org/svn/extensions/pbase2piwigo@18907 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/hu_HU/plugin.lang.php
@@ -0,0 +1,38 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Photo "%s" imported'] = 'Fotó "%s" importálva';
+$lang['Select albums to import'] = 'Albumok kiválasztása importálásra';
+$lang['Recursive'] = 'Rekurzív';
+$lang['PBase login'] = 'PBase bejelentkezés';
+$lang['No download method available. PBase2piwigo needs cURL extension or allow_url_fopen=true'] = 'Nem áll rendelkezésre letöltési mód. PBase2piwigo szüksége van cURL kiegészítőkre vagy allow_url_fopen=true';
+$lang['Invalid user id'] = 'Érvénytelen felhasználói azonosító';
+$lang['Import queue is empty'] = 'Import queue üres';
+$lang['Import in this album'] = 'Importálj ebbe az albumba';
+$lang['Fill these fields from pBase datas'] = 'Töltsd ki ezeket a mezőket a pBase adatokkal';
+$lang['Errors'] = 'Hibák';
+$lang['Completed. %1$d albums and %2$d photos added.'] = 'Kész. %2$d albumok és %3$d fotók kerültek hozzáadásra.';
+$lang['Begin transfer'] = 'Átvitel kezdés';
+$lang['Begin analyse'] = 'Elemzés kezdés';
+$lang['Album "%s" created'] = 'Album "%s" létrejött';
+$lang['%1$d errors occured. %2$d albums and %3$d photos added.'] = '%1$d hiba történt. %2$d albumok és %3$d fotók kerültek hozzáadásra.';
+?>
