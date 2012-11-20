bcfc5bdfbd8e26593b03c99b2e25d04f55400f2b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-11-20 18:33:37 +0000

    [pbase2piwigo] Add pt_BR, thanks to : glaucio
    git-svn-id: http://piwigo.org/svn/extensions/pbase2piwigo@19095 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Recursive'] = 'Recursiva';
+$lang['No download method available. PBase2piwigo needs cURL extension or allow_url_fopen=true'] = 'Nenhum método de download disponível. PBase2piwigo precisa da extensão cURL ou allow_url_fopen = true';
+$lang['Import queue is empty'] = 'Fila de importação está vazia';
+$lang['Fill these fields from pBase datas'] = 'Preencha estes campos de PBASE datas';
+$lang['%1$d errors occured. %2$d albums and %3$d photos added.'] = '%1$d erros ocorreram. %2$d álbuns e %3$d fotos foram adicionados.';
+$lang['Album "%s" created'] = 'Álbum "%s" foi criado';
+$lang['Begin analyse'] = 'Começando a análise';
+$lang['Begin transfer'] = 'Transferência iniciada';
+$lang['Completed. %1$d albums and %2$d photos added.'] = 'Completo. %1$d álbuns e %2$d fotos adicionados.';
+$lang['Errors'] = 'Erros';
+$lang['Import in this album'] = 'Importe este álbum';
+$lang['Invalid user id'] = 'ID de usuário é inválida';
+$lang['Nb albums'] = 'Nb álbuns';
+$lang['Nb photos'] = 'Nb fotos';
+$lang['PBase login'] = 'Logar PBase';
+$lang['Photo "%s" imported'] = 'foto "%s" importada';
+$lang['Select albums to import'] = 'Selecione os álbuns para importar';
+?>
