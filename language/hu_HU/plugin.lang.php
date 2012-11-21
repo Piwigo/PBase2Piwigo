43b20078024b910c2cd525914fc4ed24612d8418
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-11-21 12:34:21 +0000

    [pbase2piwigo] Update hu_HU, thanks to : samli
    git-svn-id: http://piwigo.org/svn/extensions/pbase2piwigo@19120 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/hu_HU/plugin.lang.php
+++ language/hu_HU/plugin.lang.php
@@ -20,21 +20,21 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
-$lang['Photo "%s" imported'] = 'Fotó "%s" importálva';
+$lang['Photo "%s" imported'] = '"%s" kép importálva';
 $lang['Select albums to import'] = 'Albumok kiválasztása importálásra';
 $lang['Recursive'] = 'Rekurzív';
 $lang['PBase login'] = 'PBase bejelentkezés';
-$lang['No download method available. PBase2piwigo needs cURL extension or allow_url_fopen=true'] = 'Nem áll rendelkezésre letöltési mód. PBase2piwigo szüksége van cURL kiegészítőkre vagy allow_url_fopen=true';
+$lang['No download method available. PBase2piwigo needs cURL extension or allow_url_fopen=true'] = 'Nem áll rendelkezésre letöltési mód. PBase2piwigo-nak szüksége van cURL kiegészítőkre vagy allow_url_fopen=true';
 $lang['Invalid user id'] = 'Érvénytelen felhasználói azonosító';
 $lang['Import queue is empty'] = 'Import queue üres';
 $lang['Import in this album'] = 'Importálj ebbe az albumba';
 $lang['Fill these fields from pBase datas'] = 'Töltsd ki ezeket a mezőket a pBase adatokkal';
 $lang['Errors'] = 'Hibák';
-$lang['Completed. %1$d albums and %2$d photos added.'] = 'Kész. %2$d albumok és %3$d fotók kerültek hozzáadásra.';
-$lang['Begin transfer'] = 'Átvitel kezdés';
-$lang['Begin analyse'] = 'Elemzés kezdés';
-$lang['Album "%s" created'] = 'Album "%s" létrejött';
-$lang['%1$d errors occured. %2$d albums and %3$d photos added.'] = '%1$d hiba történt. %2$d albumok és %3$d fotók kerültek hozzáadásra.';
+$lang['Completed. %1$d albums and %2$d photos added.'] = 'Kész. %2$d album és %3$d kép hozzáadva.';
+$lang['Begin transfer'] = 'Átvitel';
+$lang['Begin analyse'] = 'Vizsgálat';
+$lang['Album "%s" created'] = '"%s" album létrehozva';
+$lang['%1$d errors occured. %2$d albums and %3$d photos added.'] = '%1$d hiba történt. %2$d album és %3$d kép hozzáadva.';
 $lang['Nb photos'] = 'Db. fotó';
 $lang['Nb albums'] = 'Db. album';
 ?>
