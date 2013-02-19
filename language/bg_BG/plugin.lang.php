37099837dc13a84daf3b1ed5059c7ca882ac294e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-19 10:26:35 +0000

    [pbase2piwigo] Add bg_BG, thanks to : turnokopa
    git-svn-id: http://piwigo.org/svn/extensions/pbase2piwigo@20841 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/bg_BG/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Select albums to import'] = 'Изберете албум за импортиране';
+$lang['Recursive'] = 'Рекурсивно';
+$lang['Photo "%s" imported'] = 'Снимка "%s" импортирана';
+$lang['PBase login'] = 'PBase логин';
+$lang['No download method available. PBase2piwigo needs cURL extension or allow_url_fopen=true'] = 'Няма налична опция за сваляне. PBase2piwigo се нуждае от cURL разширение или от allow_url_fopen=true';
+$lang['Nb photos'] = 'Nb снимки';
+$lang['Nb albums'] = 'Nb албум';
+$lang['Invalid user id'] = 'Невалиден потребителски номер';
+$lang['Import queue is empty'] = 'Списъка за импортиране е празен';
+$lang['Import in this album'] = 'Импортиране в този албум';
+$lang['Fill these fields from pBase datas'] = 'Попълнете тези полета от pBase datas';
+$lang['Completed. %1$d albums and %2$d photos added.'] = 'Завършено. %1$d албума и %2$d снимки добавени.';
+$lang['Errors'] = 'Грешки';
+$lang['Begin transfer'] = 'Начало на трансфер';
+$lang['Begin analyse'] = 'Начало на анализ';
+$lang['Album "%s" created'] = 'Албум "%s" създаден';
+$lang['%1$d errors occured. %2$d albums and %3$d photos added.'] = '%1$d налични грешки. %2$d албума и %3$d снимки добавени.';
+?>
