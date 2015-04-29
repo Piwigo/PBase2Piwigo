b8cc5569d8450837daf543797c399de11143af8f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-04-29 20:09:29 +0200

    Add sv_SE, thanks to wahlis

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%1$d errors occured. %2$d albums and %3$d photos added.'] = '%1$d fel inträffade. %2$d album och %3$d bilder tillagda.';
+$lang['Album "%s" created'] = 'Album "%s" skapat';
+$lang['Begin analyse'] = 'Börja analys';
+$lang['Begin transfer'] = 'Börja överföring';
+$lang['Completed. %1$d albums and %2$d photos added.'] = 'Färdigt. %1$d album och %2$d bilder tillagda.';
+$lang['Errors'] = 'Fel';
+$lang['Fill these fields from pBase datas'] = 'Fyll i dessa fält från pBase data';
+$lang['Import in this album'] = 'Importera till detta album';
+$lang['Import queue is empty'] = 'Importkön är tom';
+$lang['Invalid user id'] = 'Ogiltigt användarid';
+$lang['Nb albums'] = 'Antal album';
+$lang['Nb photos'] = 'Antal bilder';
+$lang['No download method available. PBase2piwigo needs cURL extension or allow_url_fopen=true'] = 'Ingen nedladdningsmetod tillgänglig. PBase2piwigo behöver cURL-tillägget eller allow_url_fopen=true';
+$lang['PBase login'] = 'PBase inloggning';
+$lang['Photo "%s" imported'] = 'Photo "%s" importerat';
+$lang['Recursive'] = 'Rekursivt';
+$lang['Select albums to import'] = 'Välj album att importera';
