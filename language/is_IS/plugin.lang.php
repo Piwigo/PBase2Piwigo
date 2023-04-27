4624d46c981c12edd9287b2e1be9e89a31592de7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-04-27 18:05:07 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%1$d errors occured. %2$d albums and %3$d photos added.'] = '%1$d villur komu upp. %2$d albúmum og %3$d myndum bætt við.';
+$lang['Album "%s" created'] = 'Albúm "%s" búið til';
+$lang['Begin analyse'] = 'Byrjaðu að greina';
+$lang['Begin transfer'] = 'Hefja flutning';
+$lang['Completed. %1$d albums and %2$d photos added.'] = 'Lokið. %1$d albúmum og %2$d myndum bætt við.';
+$lang['Errors'] = 'Villur';
+$lang['Fill these fields from pBase datas'] = 'Fylltu út þessa reiti úr pBase gögnum';
+$lang['Import in this album'] = 'Flytja inn í þetta albúm';
+$lang['Import queue is empty'] = 'Innflutningsbiðröð er tóm';
+$lang['Invalid user id'] = 'Ógilt notandaauðkenni';
+$lang['Nb albums'] = 'Nb albúm';
+$lang['Nb photos'] = 'Nb myndir';
+$lang['No download method available. PBase2piwigo needs cURL extension or allow_url_fopen=true'] = 'Engin niðurhalsaðferð í boði. PBase2piwigo þarf cURL framlengingu eða allow_url_fopen=true';
+$lang['PBase login'] = 'PBase innskráning';
+$lang['Photo "%s" imported'] = 'Myndin „%s“ flutt inn';
+$lang['Recursive'] = 'Endurkvæmt';
+$lang['Select albums to import'] = 'Veldu albúm til að flytja inn';
