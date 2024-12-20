12a0057ab1a6ead3da539b2d652ab96e44dd0153
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 15:51:50 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%1$d errors occured. %2$d albums and %3$d photos added.'] = 'התרחשו %1$d שגיאות. התווספו %2$d אלבומים ו-%3$d תמונות.';
+$lang['Album "%s" created'] = 'האלבום "%s" נוצר';
+$lang['Begin analyse'] = 'התחל ניתוח';
+$lang['Begin transfer'] = 'התחל העברה';
+$lang['Completed. %1$d albums and %2$d photos added.'] = 'הושלם. התווספו %1$d אלבומים ו-%2$d תמונות.';
+$lang['Errors'] = 'שגיאות';
+$lang['Fill these fields from pBase datas'] = 'מלא את השדות הללו מנתוני pBase';
+$lang['Import in this album'] = 'יבוא לתוך האלבום הזה';
+$lang['Import queue is empty'] = 'התור ליבוא ריק';
+$lang['Invalid user id'] = 'מזהה משתמש לא חוקי';
+$lang['Nb albums'] = 'מספר אלבומים';
+$lang['Nb photos'] = 'מספר תמונות';
+$lang['No download method available. PBase2piwigo needs cURL extension or allow_url_fopen=true'] = 'אין שיטת הורדה זמינה. PBase2piwigo דורש תוספת cURL או allow_url_fopen=true';
+$lang['PBase login'] = 'התחברות ל-PBase';
+$lang['Photo "%s" imported'] = 'התמונה "%s" יובאה';
+$lang['Recursive'] = 'רקורסיבי';
+$lang['Select albums to import'] = 'בחר אלבומים ליבוא';
