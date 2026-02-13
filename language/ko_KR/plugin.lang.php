f1cde8007b317e04078e68eaf90dad8b4ea2db57
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-13 13:21:36 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,24 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%1$d errors occured. %2$d albums and %3$d photos added.'] = '%1$d개의 오류가 발생했습니다. %2$d개의 앨범과 %3$d장의 사진이 추가되었습니다.';
+$lang['Album "%s" created'] = '앨범 "%s" 생성됨';
+$lang['Begin analyse'] = '분석 시작';
+$lang['Begin transfer'] = '전송 시작';
+$lang['Completed. %1$d albums and %2$d photos added.'] = '완료됨. %1$d개의 앨범과 %2$d장의 사진이 추가되었습니다.';
+$lang['Errors'] = '오류';
+$lang['Fill these fields from pBase datas'] = 'pBase 데이터에서 이 필드 채우기';
+$lang['Import in this album'] = '이 앨범으로 가져오기';
+$lang['Import queue is empty'] = '가져오기 대기열이 비어 있습니다';
+$lang['Invalid user id'] = '잘못된 사용자 ID';
+$lang['Nb albums'] = '앨범 수';
+$lang['Nb photos'] = '사진 수';
+$lang['No download method available. PBase2piwigo needs cURL extension or allow_url_fopen=true'] = '사용 가능한 다운로드 방법이 없습니다. PBase2piwigo는 cURL 확장 모듈이 필요하거나 allow_url_fopen=true로 설정되어야 합니다';
+$lang['PBase login'] = 'PBase 로그인';
+$lang['Photo "%s" imported'] = '사진 "%s" 가져옴';
+$lang['Recursive'] = '재귀적';
+$lang['Select albums to import'] = '가져올 앨범 선택';
